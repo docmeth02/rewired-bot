a247796322ca1a540839114b7652fe490dc204ea
docmeth02 <docmeth02@googlemail.com>
2013-01-18 22:20:15 +0100

    Made greeting configurable via \!greet command

--- plugins/greet.py
+++ plugins/greet.py
@@ -5,16 +5,35 @@ class rewiredBotPlugin():
     def __init__(self, parent, *args):
         self.parent = parent
         self.defines = "!greet"
-        self.greet = False  # False , "Guests", "All"
+        self.greet = 0  # False , "Guests", "All"
         self.parent.librewired.notify("__ClientJoin", self.clientJoined)
 
-    def run(*args):
-        return 0
+    def run(self, *args):
+        if not args[0]:
+            if not self.greet:
+                return "Set to greet no one"
+            if self.greet == 'guest':
+                return "Set to greet only guests"
+            return "Set to greet everyone"
+        if args[0].strip().upper() == "OFF":
+            self.greet = 0
+            return "Okay, i will greet no one from now on!"
+        if args[0].strip().upper() == "GUEST":
+            self.greet = "guest"
+            return "Okay, i will only greet guests from now on!"
+        if args[0].strip().upper() == "ALL":
+            self.greet = "all"
+            return "Okay, i will greet everyone from now on!"
+        return "Usage: !greet off/guest/all"
 
     def clientJoined(self, msg):
+        if not self.greet:
+            return 0
         if msg[1] == 1 and self.greet:  # this is public chat
             user = self.parent.librewired.getUserByID(int(msg[0]))
             if user:
+                if user.login != 'guest' and self.greet == "guest":
+                    return 0
                 greeting = "hi %s." % user.nick
                 try:
                     if not user.ip:
@@ -33,7 +52,6 @@ class rewiredBotPlugin():
         from os.path import exists
         dbpath = "GeoLiteCity.dat"
         if not exists(dbpath):
-            print "Nope"
             return 0
         location = 0
         gloc = GeoIP(dbpath)
