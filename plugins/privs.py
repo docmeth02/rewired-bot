24a0ecffcd35ac8c7ef7b9c431f3998aedac6278
docmeth02 <docmeth02@googlemail.com>
2014-03-16 12:27:43 +0100

    Fixed broken code in privilege sub system

--- plugins/privs.py
+++ plugins/privs.py
@@ -25,22 +25,22 @@ class rewiredBotPlugin():
             user = self.getUser(param)
             if not user:
                 return "Sorry, i can't find user " + str(param)
-            if user.login in self.parent.config['guestUser']:
+            if user.login in self.parent.config['guestuser']:
                 return "Sorry, can't promote a guest user"
-            if user.login in self.parent.config['opUser']:
+            if user.login in self.parent.config['opuser']:
                 return user.login + " is already a op user"
-            if not isinstance(self.parent.config['opUser'], list):
+            if not isinstance(self.parent.config['opuser'], list):
                 self.parent.config['opUser'] = []
-            self. parent.config['opUser'].append(user.login)
+            self. parent.config['opuser'].append(user.login)
             return user.login + " is now op"
 
         if command.upper() == "!-OP":
             user = self.getUser(param)
             if not user:
                 return "Sorry, i can't find user " + str(param)
-            if not isinstance(self.parent.config['opUser'], list):
+            if not isinstance(self.parent.config['opuser'], list):
                 return str(param) + " is no op user"
-            if not user.login in self.parent.config['opUser']:
+            if not user.login in self.parent.config['opuser']:
                 return str(param) + " is no op user"
             self.parent.config['opUser'].remove(user.login)
             return "Okay, demoted user " + str(user.login)
